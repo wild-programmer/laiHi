a3849504da91990e1e0c9138fa627b5db769c5b7
wangdong <1169318664@qq.com>
2017-06-16 18:10:06 +0800

    小程序后段

--- xiaocx/pages/dizhi/dizhi.js
+++ xiaocx/pages/dizhi/dizhi.js
@@ -2,7 +2,30 @@ var app = getApp()
 Page({
   data:{
     motto:'小小科技小小科技小小科技小小科技小小科技小小科技小小科技小小科技',
-    dizhi:'上海静安'
+    dizhi:'上海静安',
+    location:'上海',
+    dz:[
+      {
+        message:'北京'
+      },
+      {
+        message:'上海'
+        },
+      {
+        message:'广州'
+        },
+      {
+        message:'深圳'
+        },
+      {
+        message:'杭州'
+        },
+      {
+        message:'郑州'
+        }
+    ]
   },
- 
+ touchendsite: function(event) {
+    this.setData({ location:event.currentTarget.id})    
+  }
 })
--- xiaocx/pages/item/item.js
+++ xiaocx/pages/item/item.js
@@ -2,3 +2,14 @@
 Page({
   data:{},
 })
+// wx.showModal({
+//   title: '提示',
+//   content: '请求成功',
+//   success: function(res) {
+//     if (res.confirm) {
+//       console.log('用户点击确定')
+//     } else if (res.cancel) {
+//       console.log('用户点击取消')
+//     }
+//   }
+// })
